0ac44c4cd54eb85ae0b234fb9628b23451742773
Oleg Kostrytsyn <okostritsyn@gmail.com>
2022-04-17 13:29:49 +0300

    add demo and class for prototype

---
+++ src/main/java/patterns/prototype/caching/cache/BundledShapeCache.java
@@ -0,0 +1,39 @@
+package main.java.patterns.prototype.caching.cache;
+
+import main.java.patterns.prototype.shapes.Circle;
+import main.java.patterns.prototype.shapes.Rectangle;
+import main.java.patterns.prototype.shapes.Shape;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BundledShapeCache {
+    private Map<String, Shape> cache = new HashMap<>();
+
+    public BundledShapeCache() {
+        Circle circle = new Circle();
+        circle.x = 5;
+        circle.y = 7;
+        circle.radius = 45;
+        circle.color = "Green";
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.x = 6;
+        rectangle.y = 9;
+        rectangle.width = 8;
+        rectangle.height = 10;
+        rectangle.color = "Blue";
+
+        cache.put("Big green circle", circle);
+        cache.put("Medium blue rectangle", rectangle);
+    }
+
+    public Shape put(String key, Shape shape) {
+        cache.put(key, shape);
+        return shape;
+    }
+
+    public Shape get(String key) {
+        return cache.get(key).clone();
+    }
+}
